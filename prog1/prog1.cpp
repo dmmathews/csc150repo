d9c9556c768ceaf45f3d511a7abbcd27e2dfd6b8
Mathews, David M <david.mathews@mines.sdsmt.edu>
2019-02-11 07:50:08 -0700

    prog1 - [ci skip] - created basic program structure

---
+++ prog1/prog1.cpp
@@ -0,0 +1,22 @@
+#pragma pack(1)
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <cctype>
+#include <fstream>
+
+
+using namespace std;
+
+//function headers here
+
+int main(int argc, char *argv[])
+{
+    //declare variables here
+
+    //main function here
+
+    return 0;
+}
+
+//write functions here
