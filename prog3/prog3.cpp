14a7d0ff3686eeb68ce1799d19946ec7b80064fc
Mathews, David M <david.mathews@mines.sdsmt.edu>
2019-04-03 00:20:07 -0600

    Formed basic structure for program

---
+++ prog3/prog3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+main(int argc, char** argv[])
+{
+    //place variables here
+
+    //check argc
+
+    //open files
+
+    //input maze
+
+    //dynamically allocate memory
+
+    //find solution
+
+    //output solution
+    
+    //free up memory
+
+
+    //close files / clean up memory
+
+    return 0;
+}
+
+//place functions here
+
+bool solveMaze(string arr[],int xpos, int ypos)
+{
+    ;
+}
+
+bool freeMemory(string arr[])
+{
+    ;
+}
