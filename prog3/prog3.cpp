47f89902d2ba3a1075334de8d399b9393bacdf1a
Mathews, David M <david.mathews@mines.sdsmt.edu>
2019-04-05 14:34:50 -0600

    minor changes to functions, check for command line cases

--- prog3/prog3.cpp
+++ prog3/prog3.cpp
@@ -6,13 +6,43 @@
 
 using namespace std;
 
-main(int argc, char** argv[])
+//function headers here
+bool solveMaze(char *arr[], int xpos, int ypos);
+bool freeMemory(char *arr[]);
+void printMaze(char *arr[], int size, ostream &out);
+
+
+
+main(int argc, char *argv[])
 {
     //place variables here
 
+    ifstream fin;
+    ofstream fout;
+
     //check argc
 
+    if (argc != 3)
+    {
+        cout << "Usage: prog3.exe, infile, outfile" << endl;
+        return 0;
+    }
+
     //open files
+    fin.open(argv[1]);
+    if (!fin.is_open())
+    {
+        cout << "Error, file " << argv[1] << " failed to open." << endl;
+        return 0;
+    }
+    fout.open(argv[2]);
+
+    if (!fout.is_open())
+    {
+        cout << "Error, file " << argv[2] << " failed to open." << endl;
+        return 0;
+    }
+
 
     //input maze
 
@@ -27,17 +57,34 @@ main(int argc, char** argv[])
 
     //close files / clean up memory
 
+    fin.close();
+    fout.close();
     return 0;
 }
 
 //place functions here
 
-bool solveMaze(string arr[],int xpos, int ypos)
+bool solveMaze(char *arr[],int xpos, int ypos)
+{
+    //check if current location is valid
+
+    //mark current location
+
+    //move each of the four avaliable directions
+
+    //unmark location
+    return false;
+}
+
+bool freeMemory(char *arr[])
 {
     ;
 }
 
-bool freeMemory(string arr[])
+void printMaze(char *arr[], int size, ostream &out)
 {
-    ;
+    for (int i = 0; i < size; i++)
+    {
+        out << arr[i];
+    }
 }
